2cf5536f019701570e56e030d139f51481e88b8a
Sascha Grunert <sgrunert@redhat.com>
2022-04-11 14:26:09 +0200

    Use async read/write for attach
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- conmon-rs/server/src/attach.rs
+++ conmon-rs/server/src/attach.rs
@@ -4,14 +4,19 @@ use nix::sys::socket::{
     bind, listen, socket, AddressFamily, SockAddr, SockFlag, SockType, UnixAddr,
 };
 use std::{
-    io::{BufReader, BufWriter, Read, Write},
     os::unix::{
         fs::PermissionsExt,
         io::{FromRawFd, RawFd},
-        net::{UnixListener, UnixStream},
+        net,
     },
     path::Path,
-    thread,
+};
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufStream},
+    net::{UnixListener, UnixStream},
+    select,
+    sync::broadcast::{self, Receiver, Sender},
+    task,
 };
 
 #[derive(Clone, Debug)]
@@ -46,43 +51,82 @@ impl Attach {
         permissions.set_mode(0o700);
 
         listen(fd, 10).context("listen on socket fd")?;
-        thread::spawn(move || Self::start_listening(fd));
+
+        const BROADCAST_CAPACITY: usize = 64;
+        // TODO: The receivers have to be passed to the container_io module and handled there.
+        let (stdin_tx, _stdin_rx) = broadcast::channel(BROADCAST_CAPACITY);
+        let (stdout_tx, _stdout_rx) = broadcast::channel(BROADCAST_CAPACITY);
+        let (stderr_tx, _stderr_rx) = broadcast::channel(BROADCAST_CAPACITY);
+        task::spawn(async move { Self::start_listening(fd, stdin_tx, stdout_tx, stderr_tx).await });
 
         Ok(Self {})
     }
 
-    fn start_listening(fd: RawFd) -> Result<()> {
+    async fn start_listening(
+        fd: RawFd,
+        stdin: Sender<String>,
+        stdout: Sender<Vec<u8>>,
+        stderr: Sender<Vec<u8>>,
+    ) -> Result<()> {
         debug!("Start listening on attach socket");
-        let listener = unsafe { UnixListener::from_raw_fd(fd) };
-        for stream in listener.incoming() {
-            thread::spawn(|| Self::handle_client(stream?));
+        let listener = UnixListener::from_std(unsafe { net::UnixListener::from_raw_fd(fd) })?;
+        loop {
+            match listener.accept().await {
+                Ok((stream, _)) => {
+                    let stdin_tx = stdin.clone();
+                    let stdout_rx = stdout.subscribe();
+                    let stderr_rx = stderr.subscribe();
+                    task::spawn(async move {
+                        if let Err(e) =
+                            Self::handle_client(stream, stdin_tx, stdout_rx, stderr_rx).await
+                        {
+                            error!("Unable to handle attach client: {:#}", e)
+                        }
+                    });
+                }
+                Err(e) => error!("Unable to accept attach stream: {}", e),
+            }
         }
-        Ok(())
     }
 
-    fn handle_client(stream: UnixStream) -> Result<()> {
+    async fn handle_client(
+        stream: UnixStream,
+        stdin: Sender<String>,
+        mut stdout: Receiver<Vec<u8>>,
+        mut stderr: Receiver<Vec<u8>>,
+    ) -> Result<()> {
         debug!("Got new attach stream connection");
 
-        let mut reader = BufReader::new(&stream);
-        let mut writer = BufWriter::new(&stream);
+        enum Pipe {
+            Stdout = 2,
+            Stderr = 3,
+        }
 
-        const BUF_SIZE: usize = 8192;
-        let mut buf = vec![0; BUF_SIZE];
-
-        // TODO: implement me
-        buf[0] = 2;
-        writer.write_all(&buf)?;
-        buf[0] = 3;
-        writer.write_all(&buf)?;
-
+        let mut io = BufStream::new(stream);
         loop {
-            match reader.read(&mut buf) {
-                Ok(n) if n > 0 => {
-                    debug!("Read {} bytes", n);
-                }
-                Err(e) => error!("Unable to read from file descriptor: {}", e),
-                _ => {}
-            }
+            let mut line = String::new();
+
+            select! {
+                res = stdout.recv() => {
+                    let mut data = res?;
+                    debug!("Got {} byte stdout data", data.len());
+                    data.insert(0, Pipe::Stdout as u8);
+                    io.write_all(&data).await.context("write stdout")?;
+                },
+                res = stderr.recv() => {
+                    let mut data = res?;
+                    debug!("Got {} byte stderr data", data.len());
+                    data.insert(0, Pipe::Stderr as u8);
+                    io.write_all(&data).await.context("write stderr")?;
+                },
+                res = io.read_line(&mut line) => {
+                    let len = res?;
+                    if len > 0 {
+                        debug!("Got stdin line of len {}", len);
+                        stdin.send(line).context("write stdin")?;
+                    }
+                },
+            };
         }
     }
 }
