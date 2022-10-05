86b0cd01412c27799e67a4c7507737deac863e10
Sascha Grunert <sgrunert@redhat.com>
2022-10-05 13:34:36 +0200

    Switch to clap v4
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- conmon-rs/server/src/config.rs
+++ conmon-rs/server/src/config.rs
@@ -1,10 +1,10 @@
 //! Configuration related structures
-use anyhow::{bail, Result};
-use clap::{AppSettings, Parser};
+use anyhow::{bail, Context, Result};
+use clap::{Parser, ValueEnum};
 use getset::{CopyGetters, Getters, Setters};
 use serde::{Deserialize, Serialize};
 use std::{fs, path::PathBuf};
-use strum::{EnumIter, EnumString, IntoEnumIterator, IntoStaticStr};
+use strum::{AsRefStr, Display, EnumIter, EnumString, IntoStaticStr};
 
 macro_rules! prefix {
     () => {
@@ -14,52 +14,53 @@ macro_rules! prefix {
 
 #[derive(CopyGetters, Debug, Deserialize, Eq, Getters, Parser, PartialEq, Serialize, Setters)]
 #[serde(rename_all = "kebab-case")]
-#[clap(
+#[command(
     after_help("More info at: https://github.com/containers/conmon-rs"),
-    global_setting(AppSettings::NoAutoVersion)
+    disable_version_flag(true)
 )]
 
 /// An OCI container runtime monitor.
 pub struct Config {
     #[get_copy = "pub"]
-    #[clap(
+    #[arg(
         default_missing_value("default"),
         env(concat!(prefix!(), "VERSION")),
         long("version"),
-        possible_values(Verbosity::iter().map(|x| x.into()).collect::<Vec<&str>>()),
+        num_args(0..=1),
         short('v'),
+        value_enum,
         value_name("VERBOSITY")
     )]
     /// Show version information, specify "full" for verbose output.
     version: Option<Verbosity>,
 
-    #[get = "pub"]
-    #[clap(
-        default_value("info"),
+    #[get_copy = "pub"]
+    #[arg(
+        default_value_t,
         env(concat!(prefix!(), "LOG_LEVEL")),
         long("log-level"),
         short('l'),
-        possible_values(["trace", "debug", "info", "warn", "error", "off"]),
+        value_enum,
         value_name("LEVEL")
     )]
     /// The logging level of the conmon server.
-    log_level: String,
+    log_level: LogLevel,
 
     #[get_copy = "pub"]
-    #[clap(
-        default_value(LogDriver::Systemd.into()),
+    #[arg(
+        default_value_t,
         env(concat!(prefix!(), "LOG_DRIVER")),
         long("log-driver"),
         short('d'),
-        possible_values(LogDriver::iter().map(|x| x.into()).collect::<Vec<&str>>()),
+        value_enum,
         value_name("DRIVER")
     )]
     /// The logging driver used by the conmon server.
     log_driver: LogDriver,
 
     #[get = "pub"]
-    #[clap(
-        default_value(""),
+    #[arg(
+        default_value(" "),
         env(concat!(prefix!(), "RUNTIME")),
         long("runtime"),
         short('r'),
@@ -69,8 +70,8 @@ pub struct Config {
     runtime: PathBuf,
 
     #[get = "pub"]
-    #[clap(
-        default_value(""),
+    #[arg(
+        default_value(" "),
         env(concat!(prefix!(), "RUNTIME_DIR")),
         long("runtime-dir"),
         value_name("RUNTIME_DIR")
@@ -79,7 +80,7 @@ pub struct Config {
     runtime_dir: PathBuf,
 
     #[get = "pub"]
-    #[clap(
+    #[arg(
         env(concat!(prefix!(), "RUNTIME_ROOT")),
         long("runtime-root"),
         value_name("RUNTIME_ROOT")
@@ -88,7 +89,7 @@ pub struct Config {
     runtime_root: Option<PathBuf>,
 
     #[get_copy = "pub"]
-    #[clap(
+    #[arg(
         env(concat!(prefix!(), "SKIP_FORK")),
         long("skip-fork"),
         value_name("SKIP_FORK")
@@ -97,12 +98,13 @@ pub struct Config {
     skip_fork: bool,
 
     #[get_copy = "pub"]
-    #[clap(
-        default_value(CgroupManager::Systemd.into()),
+    #[arg(
+        default_value_t,
         env(concat!(prefix!(), "CGROUP_MANAGER")),
         long("cgroup-manager"),
         short('c'),
-        possible_values(CgroupManager::iter().map(|x| x.into()).collect::<Vec<&str>>()),
+        //possible_values(CgroupManager::iter().map(|x| x.into()).collect::<Vec<&str>>()),
+        value_enum,
         value_name("MANAGER")
     )]
     /// Select the cgroup manager to be used
@@ -110,17 +112,64 @@ pub struct Config {
 }
 
 #[derive(
+    AsRefStr,
     Clone,
     Copy,
     Debug,
     Deserialize,
+    Display,
     EnumIter,
     EnumString,
     Eq,
-    IntoStaticStr,
     Hash,
+    IntoStaticStr,
     PartialEq,
     Serialize,
+    ValueEnum,
+)]
+#[strum(serialize_all = "lowercase")]
+/// Available log levels.
+pub enum LogLevel {
+    /// Trace level, the most verbose one.
+    Trace,
+
+    /// Debug level, less verbose than trace.
+    Debug,
+
+    /// Info level, less verbose than debug.
+    Info,
+
+    /// Warn level, less verbose than info.
+    Warn,
+
+    /// Error level, showing only errors.
+    Error,
+
+    /// Disable logging.
+    Off,
+}
+
+impl Default for LogLevel {
+    fn default() -> Self {
+        Self::Info
+    }
+}
+
+#[derive(
+    AsRefStr,
+    Clone,
+    Copy,
+    Debug,
+    Deserialize,
+    Display,
+    EnumIter,
+    EnumString,
+    Eq,
+    Hash,
+    IntoStaticStr,
+    PartialEq,
+    Serialize,
+    ValueEnum,
 )]
 #[strum(serialize_all = "lowercase")]
 /// Available verbosity levels.
@@ -133,40 +182,52 @@ pub enum Verbosity {
 }
 
 #[derive(
+    AsRefStr,
     Clone,
     Copy,
     Debug,
     Deserialize,
+    Display,
     EnumIter,
     EnumString,
     Eq,
-    IntoStaticStr,
     Hash,
+    IntoStaticStr,
     PartialEq,
     Serialize,
+    ValueEnum,
 )]
 #[strum(serialize_all = "lowercase")]
 /// Available log drivers.
 pub enum LogDriver {
-    /// Log to stdout
+    /// Use stdout as log driver.
     Stdout,
 
     /// Use systemd journald as log driver
     Systemd,
 }
 
+impl Default for LogDriver {
+    fn default() -> Self {
+        Self::Systemd
+    }
+}
+
 #[derive(
+    AsRefStr,
     Clone,
     Copy,
     Debug,
     Deserialize,
+    Display,
     EnumIter,
     EnumString,
     Eq,
-    IntoStaticStr,
     Hash,
+    IntoStaticStr,
     PartialEq,
     Serialize,
+    ValueEnum,
 )]
 #[strum(serialize_all = "lowercase")]
 /// Available cgroup managers.
@@ -176,6 +237,12 @@ pub enum CgroupManager {
 
     /// Use the cgroup filesystem to create and manage cgroups
     Cgroupfs,
+}
+
+impl Default for CgroupManager {
+    fn default() -> Self {
+        Self::Systemd
+    }
 }
 
 impl Default for Config {
@@ -191,17 +258,37 @@ const PIDFILE: &str = "pidfile";
 impl Config {
     /// Validate the configuration integrity.
     pub fn validate(&self) -> Result<()> {
-        if self.runtime().as_os_str().is_empty() {
-            bail!("--runtime flag not set")
+        const RUNTIME_FLAG: &str = "--runtime";
+        if self
+            .runtime()
+            .to_str()
+            .context(format!("{} does not parse as string", RUNTIME_FLAG))?
+            .trim()
+            .is_empty()
+        {
+            bail!("{} flag not set", RUNTIME_FLAG)
         }
-        if self.runtime_dir().as_os_str().is_empty() {
-            bail!("--runtime-dir flag not set")
+
+        const RUNTIME_DIR_FLAG: &str = "--runtime-dir";
+        if self
+            .runtime_dir()
+            .to_str()
+            .context(format!("{} does not parse as string", RUNTIME_DIR_FLAG))?
+            .trim()
+            .is_empty()
+        {
+            bail!("{} flag not set", RUNTIME_DIR_FLAG)
         }
 
         if !self.runtime().exists() {
-            bail!("runtime path '{}' does not exist", self.runtime().display())
+            bail!(
+                "{} '{}' does not exist",
+                RUNTIME_FLAG,
+                self.runtime().display()
+            )
         }
 
+        const RUNTIME_ROOT_FLAG: &str = "--runtime-root";
         if !self.runtime_dir().exists() {
             fs::create_dir_all(self.runtime_dir())?;
         }
@@ -210,7 +297,7 @@ impl Config {
             if !rr.exists() {
                 fs::create_dir_all(rr)?;
             } else if !rr.is_dir() {
-                bail!("runtime root '{}' does not exist", rr.display())
+                bail!("{} '{}' does not exist", RUNTIME_ROOT_FLAG, rr.display())
             }
         }
 
