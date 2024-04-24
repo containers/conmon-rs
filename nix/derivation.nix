ef35ef8a869f9beffaf419910641d4275d061810
Sascha Grunert <sgrunert@redhat.com>
2024-04-24 10:22:52 +0200

    Build s390x binaries using musl libc
    Building using musl until NixOS/nixpkgs#306473 is resolved.
    
    Refers to cri-o/cri-o#7911
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- nix/derivation.nix
+++ nix/derivation.nix
@@ -7,13 +7,25 @@ with pkgs; rustPlatform.buildRustPackage {
     capnproto
     protobuf
   ];
-  buildInputs = [
-    glibc
-    glibc.static
-  ];
+  buildInputs =
+    if stdenv.hostPlatform.isMusl then [
+      libunwind
+    ] else [
+      glibc
+      glibc.static
+    ];
   RUSTFLAGS = [
     "-Ctarget-feature=+crt-static"
+  ] ++ lib.optionals stdenv.hostPlatform.isMusl [
+    "-Cpanic=abort"
+    "-Clink-args=-nostartfiles"
+    "-Clink-args=-L${libunwind}/lib"
   ];
+  # Patch nix v0.27.1 for musl
+  preBuild = lib.optionalString stdenv.hostPlatform.isMusl ''
+    sed -i 's;target_arch = "s390x";target_arch = "s390x" , not(target_env = "musl");g' \
+      /build/cargo-vendor-dir/nix-0.27.1/src/sys/statfs.rs
+  '';
   stripAllList = [ "bin" ];
   cargoLock = {
     lockFile = lib.cleanSource ./.. + "/Cargo.lock";
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -2,4 +2,6 @@ let
   static = import ./static.nix;
 in
 self: super:
-{ }
+{
+  libunwind = (static super.libunwind);
+}
